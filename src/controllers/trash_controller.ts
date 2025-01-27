d7ba6b23d88cbb5c645b93dbadbc1a26b22a9bc6
Iroegbu David <iroegbu.dgit@gmail.com>
2025-01-27 11:42:57 +0100

    new commit

--- src/controllers/trash_controller.ts
+++ src/controllers/trash_controller.ts
@@ -55,6 +55,8 @@ export const all_paginated_trash = async(req: CustomRequest, res: Response)=>{
 
 export const delete_selected_trash = async (req:CustomRequest, res: Response) => {
     try {
+
+
         const user_id = req.account_holder.user.user_id;   const role = req.account_holder.user.is_admin
 
         const {trash_id} = req.params
@@ -74,30 +76,40 @@ export const delete_selected_trash = async (req:CustomRequest, res: Response) =>
         //     return res.status(401).json({err: `Only ${trash_exist?.deleted_by?.first_name} ${trash_exist?.deleted_by?.last_name} is authorized to delete selected file.`})
         // }
 
+        let del_user;
+        let del_trash;
+
         if (trash_exist.deleted_project_id){
             await Promise.all([
                 prisma.trash.delete({where:{trash_id}}),
                 prisma.activity.deleteMany({where:{project_id:trash_exist.deleted_project_id}}),
                 prisma.project.delete({where: {project_id: trash_exist.deleted_project_id}}),
                 prisma.projectAssignment.deleteMany({where: {project_id: trash_exist.deleted_project_id}})
-
+                
             ])
         }else if (trash_exist.deleted_user_id){
 
-            await Promise.all([
-                prisma.trash.delete({where:{trash_id}}),
-                prisma.user.delete({where: {user_id: trash_exist.deleted_user_id}})
-            ])
+            await prisma.$transaction(async (prisma) => {
+                await prisma.trash.delete({ where: { trash_id } });
+                await prisma.user.delete({ where: { user_id: trash_exist.deleted_user_id || '' } });
+            });
         }
+
+        console.log('delted file ', del_user)
 
         return res.status(200).json({
             msg: 'Trash has been permanently deleted'
         })
 
-    } catch (err:any) {
-        console.log('Error deleting trash ', err);
-        return res.status(500).json({err: 'Error deleting trash ', error: err});
-    }
+    } catch (err: any) {
+        if (err.code === 'P2003') { // Foreign key constraint error
+            return res.status(400).json({
+                err: 'Cannot delete user due to been assigned to a project',
+            });
+        }
+        console.error('Error deleting trash or user:', err);
+        return res.status(500).json({ err: 'Error deleting trash or user', error: err.message });
+    }    
 }
 
 export const restore_selected_trash = async (req:CustomRequest, res: Response) => {
